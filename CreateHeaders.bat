16d5c07f7504f7f86695785ce85680842f3910e8
Alan Evans <thealanevans@googlemail.com>
2015-08-29 12:02:01 +0100

    Fix createHeaders.bat

--- CreateHeaders.bat
+++ CreateHeaders.bat
@@ -1,4 +1,1 @@
-cd bin/classes/
-javah -o ../../jni/jniExports.h com.example.nativeopengl.LibraryClass
-cd ../..
-pause
+"%JAVA_HOME%\bin\javah" -o app\src\main\jni\jniExports.h -classpath app\build\intermediates\classes\debug com.example.nativeopengl.LibraryClass
